8f5b208827606c086a695fba8be53b81c0825871
wikmor <67591683+wikmor@users.noreply.github.com>
2022-02-22 15:22:16 +0100

    Update

---
+++ src/main/java/me/wikmor/lpc/LPChatPlugin.java
@@ -0,0 +1,106 @@
+package me.wikmor.lpc;
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.cacheddata.CachedMetaData;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public final class LPChatPlugin extends JavaPlugin implements Listener {
+
+	private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
+
+	private LuckPerms luckPerms;
+	
+	@Override
+	public void onEnable() {
+		// Load an instance of 'LuckPerms' using the services manager.
+		this.luckPerms = getServer().getServicesManager().load(LuckPerms.class);
+
+		saveDefaultConfig();
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+		if (args.length == 1 && "reload".equals(args[0])) {
+			reloadConfig();
+
+			sender.sendMessage(colorize("&aLPC has been reloaded."));
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public List<String> onTabComplete(final CommandSender sender, final Command command, final String alias, final String[] args) {
+		if (args.length == 1)
+			return Collections.singletonList("reload");
+
+		return new ArrayList<>();
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onChat(final AsyncPlayerChatEvent event) {
+		final String message = event.getMessage();
+		final Player player = event.getPlayer();
+
+		// Get a LuckPerms cached metadata for the player.
+		final CachedMetaData metaData = this.luckPerms.getPlayerAdapter(Player.class).getMetaData(player);
+		final String group = metaData.getPrimaryGroup();
+
+		String format = getConfig().getString(getConfig().getString("group-formats." + group) != null ? "group-formats." + group : "chat-format")
+				.replace("{prefix}", metaData.getPrefix() != null ? metaData.getPrefix() : "")
+				.replace("{suffix}", metaData.getSuffix() != null ? metaData.getSuffix() : "")
+				.replace("{prefixes}", metaData.getPrefixes().keySet().stream().map(key -> metaData.getPrefixes().get(key)).collect(Collectors.joining()))
+				.replace("{suffixes}", metaData.getSuffixes().keySet().stream().map(key -> metaData.getSuffixes().get(key)).collect(Collectors.joining()))
+				.replace("{world}", player.getWorld().getName())
+				.replace("{name}", player.getName())
+				.replace("{displayname}", player.getDisplayName())
+				.replace("{username-color}", metaData.getMetaValue("username-color") != null ? metaData.getMetaValue("username-color") : "")
+				.replace("{message-color}", metaData.getMetaValue("message-color") != null ? metaData.getMetaValue("message-color") : "");
+
+		format = translateHexColorCodes(colorize(getServer().getPluginManager().isPluginEnabled("PlaceholderAPI") ? PlaceholderAPI.setPlaceholders(player, format) : format));
+
+		event.setFormat(format.replace("{message}", player.hasPermission("lpc.colorcodes") && player.hasPermission("lpc.rgbcodes")
+				? translateHexColorCodes(colorize(message)) : player.hasPermission("lpc.colorcodes") ? colorize(message) : player.hasPermission("lpc.rgbcodes")
+				? translateHexColorCodes(message) : message).replace("%", "%%"));
+	}
+
+	private String colorize(final String message) {
+		return ChatColor.translateAlternateColorCodes('&', message);
+	}
+
+	private String translateHexColorCodes(final String message) {
+		final char colorChar = ChatColor.COLOR_CHAR;
+
+		final Matcher matcher = HEX_PATTERN.matcher(message);
+		final StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+
+		while (matcher.find()) {
+			final String group = matcher.group(1);
+
+			matcher.appendReplacement(buffer, colorChar + "x"
+					+ colorChar + group.charAt(0) + colorChar + group.charAt(1)
+					+ colorChar + group.charAt(2) + colorChar + group.charAt(3)
+					+ colorChar + group.charAt(4) + colorChar + group.charAt(5));
+		}
+
+		return matcher.appendTail(buffer).toString();
+	}
+}
